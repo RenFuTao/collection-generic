ea5880a8405bad1c523c5e567d2b07cef7499c5c
RIsak <813487414@qq.com>
2019-05-28 21:50:46 +0800

    test4(1)

---
+++ src/test4/MyQuene.java
@@ -0,0 +1,26 @@
+package test4;
+
+import java.util.LinkedList;
+
+public class MyQuene {
+	LinkedList lists = new LinkedList();
+	public void push(Object obj) {
+		lists.addLast(obj);
+	}
+	public Object pop() {
+		return lists.removeFirst();
+	}
+	public String toString() {
+		return lists.toString();
+	}
+	public static void main(String[] args) {
+		MyQuene mq = new MyQuene();
+		mq.push("Jack");
+		mq.push("Fiona");
+		mq.push("Tom");
+		System.out.println(mq);
+		System.out.println(mq.pop());
+		System.out.println(mq);
+		
+	}
+}
